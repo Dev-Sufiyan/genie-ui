960b00ebb52e545d5d8753573043d8ae8966310e
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-08-31 01:28:46 +0530

    👆 Added a Application Drawer Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import './App.css';
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';import './App.css';
+import { Drawer} from './components/layout/nav';
+import { DrawerItem } from './types';
 
+import {
+  Board20Filled,
+  Board20Regular,
+  MegaphoneLoud20Filled,
+  MegaphoneLoud20Regular,
+  bundleIcon,
+} from "@fluentui/react-icons";
+
+// Define the icons using bundleIcon
+const DashboardIcon = bundleIcon(Board20Filled, Board20Regular);
+const AnnouncementsIcon = bundleIcon(MegaphoneLoud20Filled, MegaphoneLoud20Regular);
+
+// Define the array of items to pass to the Drawer component
+const navItems: DrawerItem[] = [
+  { href: "/dashboard", icon: <DashboardIcon />, text: "Dashboard", value: "1" },
+  { href: "/announcements", icon: <AnnouncementsIcon />, text: "Announcements", value: "2" },
+  // Add more items as needed
+];
 function App() {
   return (
-    <div> Work in Progress</div>
+    <FluentProvider theme={webLightTheme}>
+      <Drawer items={navItems}/>
+    </FluentProvider>
   );
 }
 
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export type{DrawerItem} from './drawerItem';
