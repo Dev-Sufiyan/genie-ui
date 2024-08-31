f0553e814263a7aa9f87d2d6a60005f3bcc5ad26
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-08-31 20:16:04 +0530

    Removed Unused Ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';import './App.css';
 import { Drawer} from './components/layout/nav';
 import { navItems } from './custome';
-import {AppRoutes} from './routes';
 
 function App() {
   return (
