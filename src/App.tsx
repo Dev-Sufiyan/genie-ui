11ba13365713b284d9def9049263d987bb1a883d
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-10-24 22:43:52 +0530

    ➕ Added Serachable DD

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 import { BrowserRouter as Router } from "react-router-dom";
 import { useState } from "react";
 import { AppRoutes } from "./routes";
-import './App.css'
+import "./App.css";
+import "./styles/mobilePage.css";
 
 const App: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
   return (
     <FluentProvider theme={webLightTheme}>
-      <Router>
-        <AppRoutes
-          isAuthenticated={isAuthenticated}
-          setIsAuthenticated={setIsAuthenticated}
-        />
-      </Router>
+      <div className="page-container">
+        <div className="page-border">
+        <Router>
+          <AppRoutes
+            isAuthenticated={isAuthenticated}
+            setIsAuthenticated={setIsAuthenticated}
+          />
+        </Router>
+        </div>
+      </div>
     </FluentProvider>
   );
 };
